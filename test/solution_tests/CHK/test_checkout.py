f032f51c9cedba69c5f4a2b7280c5c83154180bd
sbx_user1051 <sbx_user1051@169.254.14.138>
2024-12-18 11:28:15 +0000

    Wed Dec 18 11:28:15 UTC 2024

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -5,6 +5,10 @@ class TestSuperstore():
     def test_without_discount(self):
         assert checkout_solution.checkout(["C", "C", "D"]) == 55
     
-    def test_with_discount_whole_numbers(self):
+    def test_with_discount_whole_discounts(self):
         assert checkout_solution.checkout(["A", "A", "A", "B", "B"]) == 175
+
+    def test_with_discount_fraction_discounts(self):
+        assert checkout_solution.checkout(["A", "A", "A", "A", "B", "B", "C"]) == 245
+
 
